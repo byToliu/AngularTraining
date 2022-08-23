24ef1a51a01c07f87395842c29a59b7c6a12e585
Tulio Santos <edd.di@unip.br>
2022-08-23 18:39:08 -0300

    Criação de um componente manualmente e com angular CLI

---
+++ primeiroProjeto/src/app/primeiro-componente/primeiro-componente.component.ts
@@ -0,0 +1,10 @@
+import { Component} from '@angular/core';
+
+@Component({
+    selector: 'primeiro-componente',
+    template: `
+        <p>Nice!</p>  
+    `
+})
+
+export class PrimeiroComponenteComponent {}
